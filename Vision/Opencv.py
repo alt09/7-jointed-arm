fee0d76c5e145eaba656752adfb23ce529a6f00b
Alt09 <andreslaratrevino@gmail.com>
2026-09-04 19:56:54 -0400

    fix cam issue

--- Vision/Opencv.py
+++ Vision/Opencv.py
@@ -6,19 +6,23 @@ def moments(rgba_img,lower_color,upper_color):
 
     bgr_img = cv2.cvtColor(rgba_img, cv2.COLOR_RGBA2BGR)
     hsv = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
-    lower_color = np.array(lower_color) # HSV color
-    upper_color = np.array(upper_color) # HSV color
-    mask = cv2.inRange(hsv, lower_color, upper_color)    
+    mask = cv2.inRange(hsv, np.array(lower_color), np.array(upper_color))    
 
     M = cv2.moments(mask)
     return M
 def center_of_mass(rgba_img, lower_color,upper_color):
     bgr_img = cv2.cvtColor(rgba_img, cv2.COLOR_RGBA2BGR)
-    M = moments(bgr_img, lower_color, upper_color)
+    hsv = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, np.array(lower_color), np.array(upper_color))
+
+    M = cv2.moments(mask)
     if M["m00"] != 0:
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
+        #print(f"Detected red object at ({cX}, {cY})")
 
         cv2.circle(bgr_img, (cX, cY), 5, (0, 255, 0), -1)
 
     cv2.imshow("Camera Feed", bgr_img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        return None
