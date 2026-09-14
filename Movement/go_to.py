Fix a bug that i didn't notice until now, joint 3 was moving in the wrong axis, fixed this by making the first joint aFixed joint instead of a revolute and chaniging the axis on the 3rd joint, now theres a new problem that the camera output is sending incorrect mesurements, i belive is because i set the position to the last joint but now that is fixed is wrong, also I should deleate the base link because is waisting resources and is litteraly doing nothing. so goal for next commit is deleting the base_link and the first joint. After that is fixing camera output

Movement/go_to.py
@@ -3,7 +3,19 @@ from Vision import opencv
 import sim
 import math
 import numpy as np
-from scipy.spatial.transform import Rotation as R
+
+def inverse_kinematics(arm_id, target_position, target_orientation):
+    """
+    Computes the inverse kinematics for the robotic arm to reach a target position and orientation.
+    Args:
+        arm_id (int): The ID of the robotic arm in the PyBullet simulation.
+        target_position (list): A list of 3 coordinates [x, y, z] representing the target position in 3D space.
+        target_orientation (list): A list of 4 coordinates [x, y, z, w] representing the target orientation as a quaternion.
+    Returns:
+        list: A list of joint angles for the robotic arm to reach the target position and orientation.
+    """
+
+    target_joint_positions = []
 def go_to(arm_id, num_joints, go_to):
     """
     Moves the robotic arm to the specified joint positions.
@@ -15,30 +27,30 @@ def go_to(arm_id, num_joints, go_to):
     for i in range(num_joints):
         sim.set_joint_positions(i, go_to[i], arm_id)
 
-def go_to_target(arm_id,target_position):
+def go_to_target(arm_id,target_position, target_orientation):
     """
     Moves the robotic arm to the specified target position.
     Args:
         arm_id (int): The ID of the robotic arm in the PyBullet simulation.
         target_position (list): A list of 3 target coordinates [x, y, z] for the end effector.
-        targetOrientation (list): A list of 4 target orientation values [x, y, z, w] for the end effector.
+        target_orientation (list): A list of 4 target orientation values [x, y, z, w] for the end effector.
     """
 
     # Calculate the inverse kinematics to find the joint angles for the target position
-    target_joint_positions = sim.p.calculateInverseKinematics(arm_id, 7, target_position)  # 7 is the index of the end effector link
+    target_joint_positions = sim.p.calculateInverseKinematics(arm_id, 7, target_position, target_orientation)  # 7 is the index of the end effector link
 
     # print(f"Target joint positions: {target_joint_positions}")
 
     # Move the arm to the target joint positions
-    for i in range(sim.p.getNumJoints(arm_id)):
-        sim.set_joint_positions(i, target_joint_positions[i], arm_id)
+    for i in range(sim.p.getNumJoints(arm_id)-1):
+        sim.set_joint_positions(i+1, target_joint_positions[i], arm_id)
 def where_is(arm_id):
     """
     Returns the current position of the end effector of the robotic arm.
     Args:
         arm_id (int): The ID of the robotic arm in the PyBullet simulation.
     Returns:
-        list: A list of 3 coordinates [x, y, z] representing the current position of the end effector.
+        list: A list of 2 values [End_effector_position, End_effector_orientation] representing the current position and orientation of the end effector.
     """
     quaternion = sim.p.getLinkState(arm_id, 7)[5]  # Get the orientation of the end effector
 
@@ -82,4 +94,22 @@ def cheats(arm_id,target_3Dposition,viewMatrix1,viewMatrix2):
         wrist_pitch = utils.Yaw_pitch_roll_from_quaternion(sim.p.getLinkState(arm_id, 6)[5])[1]  # Get the current position of the wrist
         sim.set_joint_positions(7,-(end_effector_yaw+angle[0]), arm_id) # izquierda Move the arm to the calculated joint positions
         sim.set_joint_positions(6,-(wrist_pitch+angle[1]), arm_id) # abajo
+
+def approach(arm_id,target_3Dposition,viewMatrix1,viewMatrix2):
+    """
+    Moves the robotic arm to a predefined approach position.
+    Args:
+        arm_id (int): The ID of the robotic arm in the PyBullet simulation.
+        target_3Dposition (list): A list of 3 coordinates [x, y, z] representing the target position in 3D space.
+        viewMatrix1 (list): The view matrix of the first camera.
+        viewMatrix2 (list): The view matrix of the second camera.
+    Returns:
+        None
+    """
+    target_approach_position = target_3Dposition - np.array([1, 1, 0])  # Move 10 cm above the target position
+    
+    if target_3Dposition is not None:
+        angle = auto_aim(target_approach_position,viewMatrix1,viewMatrix2)  # Get the final angle from auto_aim
+        go_to_target(arm_id, target_3Dposition, utils.quaternion_from_yaw_pitch_roll(angle[0], angle[1], 0))  # Move to a predefined approach position
+        print("target_3Dposition",target_3Dposition)
 