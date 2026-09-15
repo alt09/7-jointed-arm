952f67a4efbb5c08cb655094838572b8d2a27cd8
Alt09 <andreslaratrevino@gmail.com>
2026-09-15 14:35:17 -0400

    fix the arm.urdf file so it looks better

--- Movement/kinematics.py
+++ Movement/kinematics.py
@@ -1,5 +1,7 @@
 import robot_controller
 import Utils.utils as utils
+import numpy as np
+import math
 def inverse_kinematics(arm_id, target_position, target_orientation):
     """
     Computes the inverse kinematics for the robotic arm to reach a target position and orientation.
@@ -12,3 +14,14 @@ def inverse_kinematics(arm_id, target_position, target_orientation):
     """
 
     target_joint_positions = []
+def forward_kinematics(arm_id, joint_angles):
+    """
+    Computes the forward kinematics for the robotic arm given a set of joint angles(in radians). 
+    Args:
+        arm_id (int): The ID of the robotic arm in the PyBullet simulation.
+        joint_angles (list): A list of joint angles for the robotic arm(in radians).
+    Returns:
+        list: A list of 3 coordinates [x, y, z] representing the position of the end effector in 3D space.
+    """
+    # Placeholder for forward kinematics implementation
+    pass
--- sim.py
+++ sim.py
@@ -78,17 +78,17 @@ def sim():
         opencv.center_of_mass(rgba_img2, [0, 0, 55], [0, 0, 255],"Right")
 
        # go_to.go_to_target(arm_id, [4, 0, 1])
-        if opencv.target_3d_pose(viewMatrix1,viewMatrix2,projectionMatrix,rgba_img1,rgba_img2,[0, 0, 55], [0, 0, 255]) is not None:
-            target_last_info = opencv.target_3d_pose(viewMatrix1,viewMatrix2,projectionMatrix,rgba_img1,rgba_img2,[0, 0, 55], [0, 0, 255])
-            if target_last_info[1] < 0.2:
-                robot_controller.approach(arm_id,target_last_info[0],viewMatrix1,viewMatrix2)
-                target_last_good_pose=target_last_info[0]
-        else:
-           # go_to.approach(arm_id,target_last_pose,viewMatrix1,viewMatrix2)
-           if target_last_info is not None:
-                robot_controller.approach(arm_id,target_last_good_pose,viewMatrix1,viewMatrix2)
-                print("target_last_pose good",target_last_good_pose)
-                print("target_last_posebad?",target_last_info[1])
+        # if opencv.target_3d_pose(viewMatrix1,viewMatrix2,projectionMatrix,rgba_img1,rgba_img2,[0, 0, 55], [0, 0, 255]) is not None:
+        #     target_last_info = opencv.target_3d_pose(viewMatrix1,viewMatrix2,projectionMatrix,rgba_img1,rgba_img2,[0, 0, 55], [0, 0, 255])
+        #     if target_last_info[1] < 0.2:
+        #         robot_controller.approach(arm_id,target_last_info[0],viewMatrix1,viewMatrix2)
+        #         target_last_good_pose=target_last_info[0]
+        # else:
+        #    # go_to.approach(arm_id,target_last_pose,viewMatrix1,viewMatrix2)
+        #    if target_last_info is not None:
+        #         robot_controller.approach(arm_id,target_last_good_pose,viewMatrix1,viewMatrix2)
+        #         print("target_last_pose good",target_last_good_pose)
+        #         print("target_last_posebad?",target_last_info[1])
 
 def get_joint_info(arm_id):
     """
